2cd5a40ff3433624e1628b8d4746bdd4cfbb5b83
Oriana <ori@oriana.com>
2021-05-08 20:53:22 -0400

    3. Material, Schematic, parte visual lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,6 +39,10 @@ const routes: Routes = [
     ]
   },
   {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
     path: '**',
     component: PageNotFondComponent
   }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,12 +7,13 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CartComponent } from './components/cart/cart.component';
-
+import { MaterialModule } from './../material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ],
   declarations: [
     ExponentialPipe,
